79091a2476bc90f1721ae8064ebec230a10e1f09
Emma Lin <line@meta.com>
2025-10-28 20:43:26 -0700

    enable feature score auto collection in EBC (#3475)
    Summary:
    X-link: https://github.com/pytorch/FBGEMM/pull/5031
    
    
    X-link: https://github.com/facebookresearch/FBGEMM/pull/2044
    
    Enable feature score auto collection for EBC in the similar way of EC. The configuration has no difference in embedding table config:
    
                  virtual_table_eviction_policy=FeatureScoreBasedEvictionPolicy(
                      training_id_eviction_trigger_count=260_000_000,  # 260M
                      training_id_keep_count=160_000_000,  # 160M
                      enable_auto_feature_score_collection=True,
                      feature_score_mapping={
                          "sparse_public_original_content_creator": 1.0,
                      },
                      feature_score_default_value=0.5,
                  ),
    
    Differential Revision: D85017179

--- torchrec/distributed/embedding_lookup.py
+++ torchrec/distributed/embedding_lookup.py
@@ -66,6 +66,7 @@ from torchrec.distributed.quant_embedding_kernel import (
     QuantBatchedEmbeddingBag,
 )
 from torchrec.distributed.types import rank_device, ShardedTensor, ShardingType
+from torchrec.modules.embedding_configs import FeatureScoreBasedEvictionPolicy
 from torchrec.sparse.jagged_tensor import KeyedJaggedTensor
 
 logger: logging.Logger = logging.getLogger(__name__)
@@ -515,6 +516,23 @@ class GroupedPooledEmbeddingsLookup(
     ) -> None:
         super().__init__()
         self._emb_modules: nn.ModuleList = nn.ModuleList()
+        self._feature_score_auto_collections: List[bool] = []
+        for config in grouped_configs:
+            collection = False
+            for table in config.embedding_tables:
+                if table.use_virtual_table and isinstance(
+                    table.virtual_table_eviction_policy, FeatureScoreBasedEvictionPolicy
+                ):
+                    if (
+                        table.virtual_table_eviction_policy.enable_auto_feature_score_collection
+                    ):
+                        collection = True
+            self._feature_score_auto_collections.append(collection)
+
+        logger.info(
+            f"GroupedPooledEmbeddingsLookup: {self._feature_score_auto_collections=}"
+        )
+
         for config in grouped_configs:
             self._emb_modules.append(
                 self._create_embedding_kernel(config, device, pg, sharding_type)
@@ -692,8 +710,11 @@ class GroupedPooledEmbeddingsLookup(
             features_by_group = sparse_features.split(
                 self._feature_splits,
             )
-            for config, emb_op, features in zip(
-                self.grouped_configs, self._emb_modules, features_by_group
+            for config, emb_op, features, fs_auto_collection in zip(
+                self.grouped_configs,
+                self._emb_modules,
+                features_by_group,
+                self._feature_score_auto_collections,
             ):
                 if (
                     config.has_feature_processor
@@ -703,9 +724,19 @@ class GroupedPooledEmbeddingsLookup(
                     features = self._feature_processor(features)
 
                 if config.is_weighted:
-                    features._weights = CommOpGradientScaling.apply(
+                    feature_weights = CommOpGradientScaling.apply(
                         features._weights, self._scale_gradient_factor
-                    )
+                    ).float()
+
+                    if fs_auto_collection and features.weights_or_none() is not None:
+                        score_weights = features.weights().float()
+                        assert (
+                            feature_weights.numel() == score_weights.numel()
+                        ), f"feature_weights.numel() {feature_weights.numel()} != score_weights.numel() {score_weights.numel()}"
+                        cat_weights = torch.cat(
+                            [feature_weights, score_weights], dim=1
+                        ).view(torch.float64)
+                        features._weights = cat_weights
 
                 lookup = emb_op(features)
                 embeddings.append(lookup)
