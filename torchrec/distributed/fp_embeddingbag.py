33ee996c90edc9026ce144f7e8aecbc596737e38
Srikanth Kamath <srkamath@meta.com>
2026-01-30 21:40:04 -0800

    Back out D91704824: fix torch.package backward compatibility (#3709)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchrec/pull/3709
    
    Release blocker: https://www.internalfb.com/ci/results/slp/668555599615864?tag=conveyor_artifact%3A%3A%2F%2Ffblearner%2Fflow%2Fprojects%2Flego%3Alego_flow
    
    D91704824 introduced `from torchrec.pt2.utils import pt2_compatible_justknobs_check` in several torchrec modules. This import breaks torch.package backward compatibility during repackaging because old packages on Manifold have bundled `torchrec.pt2.utils` that doesn't contain the new `pt2_compatible_justknobs_check` function, causing ImportError.
    
    The author should address the backward compatibility issue before re-landing by either:
    1. Adding the import inside a try/except block
    2. Adding `torchrec.pt2.**` to extern_modules in the packager
    
    Reviewed By: amrshennawi, TroyGarden
    
    Differential Revision: D91941786
    
    fbshipit-source-id: 3c8f4d2dc5049844944b26ba6008dcce039927af

--- torchrec/distributed/fp_embeddingbag.py
+++ torchrec/distributed/fp_embeddingbag.py
@@ -51,7 +51,6 @@ from torchrec.modules.fp_embedding_modules import (
     apply_feature_processors_to_kjt,
     FeatureProcessedEmbeddingBagCollection,
 )
-from torchrec.pt2.utils import pt2_compatible_justknobs_check
 from torchrec.sparse.jagged_tensor import KeyedJaggedTensor, KeyedTensor
 
 _T = TypeVar("_T")
@@ -123,7 +122,7 @@ class ShardedFeatureProcessedEmbeddingBagCollection(
     def input_dist(
         self, ctx: EmbeddingBagCollectionContext, features: KeyedJaggedTensor
     ) -> Awaitable[Awaitable[KJTList]]:
-        if pt2_compatible_justknobs_check(
+        if torch._utils_internal.justknobs_check(
             "pytorch/torchrec:enable_rw_feature_processor"
         ):
             if not self.is_pipelined and self._row_wise_sharded:
@@ -286,7 +285,7 @@ class FeatureProcessedEmbeddingBagCollectionSharder(
             ShardingType.TABLE_COLUMN_WISE.value,
         ]
 
-        if pt2_compatible_justknobs_check(
+        if torch._utils_internal.justknobs_check(
             "pytorch/torchrec:enable_rw_feature_processor"
         ):
             types.extend(
