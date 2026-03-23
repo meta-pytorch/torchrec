9a05f48d348261e626c8bd065c33aabb5ccbabf1
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-03-23 02:12:58 -0700

    Fix Unused Import issues in fbcode/torchrec/distributed/benchmark/benchmark_zch/data/preprocess/__init__.py (#3911)
    Summary: Pull Request resolved: https://github.com/meta-pytorch/torchrec/pull/3911
    
    Reviewed By: rexyl
    
    Differential Revision: D97463932
    
    fbshipit-source-id: d0d2f7b509c19f2d65033895f6c54e5a803b912e

--- torchrec/distributed/benchmark/benchmark_zch/data/preprocess/__init__.py
+++ torchrec/distributed/benchmark/benchmark_zch/data/preprocess/__init__.py
@@ -8,5 +8,5 @@
 # pyre-strict
 from .criteo_kaggle import get_criteo_kaggle_dataloader  # noqa: F401
 from .kuairand_1k import get_kuairand_1k_dataloader  # noqa: F401
-from .kuairand_27k import get_kuairand_27k_dataloader
-from .movielens_1m import get_movielens_1m_dataloader
+from .kuairand_27k import get_kuairand_27k_dataloader  # noqa: F401
+from .movielens_1m import get_movielens_1m_dataloader  # noqa: F401
