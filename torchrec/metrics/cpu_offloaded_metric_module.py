055a3ac30341548a4d18772bd91eed2c8d6e662f
Jeff Kim <jeffkim@meta.com>
2026-03-30 10:20:20 -0700

    Make DeferrableMetrics implement Mapping[str, Any] (#3944)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchrec/pull/3944
    
    Makes `DeferrableMetrics` implement `Mapping[str, Any]` so it's a
    runtime drop-in for `Dict[str, MetricValue]`.
    
    Reviewed By: sahshah-meta
    
    Differential Revision: D98336705
    
    fbshipit-source-id: 961414aab1c01672b51505b1bf2cd869ed01e2da

--- torchrec/metrics/cpu_offloaded_metric_module.py
+++ torchrec/metrics/cpu_offloaded_metric_module.py
@@ -18,12 +18,13 @@ from torch import distributed as dist
 from torch.monitor import _WaitCounter
 from torch.profiler import record_function
 from torchrec.metrics.cpu_comms_metric_module import CPUCommsRecMetricModule
+from torchrec.metrics.deferrable_metrics import DeferrableMetrics
 from torchrec.metrics.metric_job_types import (
     MetricComputeJob,
     MetricUpdateJob,
     SynchronizationMarker,
 )
-from torchrec.metrics.metric_module import MetricsFuture, MetricsResult, RecMetricModule
+from torchrec.metrics.metric_module import MetricsResult, RecMetricModule
 from torchrec.metrics.metric_state_snapshot import MetricStateSnapshot
 from torchrec.metrics.model_utils import parse_task_model_outputs
 from torchrec.metrics.rec_metric import RecMetricException
@@ -270,13 +271,13 @@ class CPUOffloadedRecMetricModule(RecMetricModule):
         logger.info("CPUOffloadedRecMetricModule has been successfully shutdown.")
 
     @override
-    def compute(self) -> MetricsResult:
+    def compute(self) -> DeferrableMetrics:
         raise RecMetricException(
             "CPUOffloadedRecMetricModule does not support compute(). Use async_compute() instead."
         )
 
     @override
-    def async_compute(self) -> MetricsFuture:
+    def async_compute(self) -> DeferrableMetrics:
         """
         Entry point for asynchronous metric compute. It enqueues a synchronization marker
         to the update queue.
@@ -290,7 +291,7 @@ class CPUOffloadedRecMetricModule(RecMetricModule):
             metrics_future.set_exception(
                 RecMetricException("metric processor thread is shut down.")
             )
-            return metrics_future
+            return DeferrableMetrics(metrics_future)
 
         if self._captured_exception_event.is_set():
             assert self._captured_exception is not None
@@ -298,7 +299,7 @@ class CPUOffloadedRecMetricModule(RecMetricModule):
 
         self.update_queue.put_nowait(SynchronizationMarker(metrics_future))
         self.update_queue_size_logger.add(self.update_queue.qsize())
-        return metrics_future
+        return DeferrableMetrics(metrics_future)
 
     def _process_synchronization_marker(
         self, synchronization_marker: SynchronizationMarker
@@ -361,7 +362,7 @@ class CPUOffloadedRecMetricModule(RecMetricModule):
 
                 with record_function("## metric_compute ##"):
                     compute_start_ms = time.time()
-                    computed_metrics = self.comms_module.compute()
+                    computed_metrics = self.comms_module.compute().resolve()
                     self.compute_job_time_logger.add((time.time() - start_ms) * 1000)
                     self.compute_metrics_time_logger.add(
                         (time.time() - compute_start_ms) * 1000
