06cf6a82c945c12de5d21ecfa92d97f2a9c29751
pytorchbot <pytorchbot@pytorch.com>
2026-08-10 11:48:30 +0000

    2026-08-10 nightly release (2729552b6095ad3a4c9bbe93d76319d4162cb98a)

--- .github/scripts/filter.py
+++ .github/scripts/filter.py
@@ -64,9 +64,9 @@ def main():
             # fbgemm only supports cuda 12.6, 12.8, and 13.0
             # Re-enable once fbgemm-gpu releases cu132 nightly builds
             continue
-        if entry["python_version"] in ("3.9"):
-            # stop python3.9 support
-            # for python version: https://devguide.python.org/versions/
+        if entry["python_version"] in ("3.15", "3.15t"):
+            # fbgemm-gpu does not support python3.15 yet
+            # Re-enable once fbgemm-gpu releases python3.15 nightly builds
             continue
         new_matrix_entries.append(entry)
 
