70dfd94d40e3e3fd8a8b5c74f3dfb2f8b35b00d9
Shuangping Liu <spliu@meta.com>
2026-08-16 13:32:05 -0700

    Register PECEmbeddingCollectionSharder in default sharders (#4578)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/torchrec/pull/4578
    
    ## Context
    
    `PECEmbeddingCollection` could not be sharded by `DistributedModelParallel` / the planner out of the box -- callers had to pass the PEC sharder explicitly.
    
    ## Approach
    
    - Add `PECEmbeddingCollectionSharder` to `get_default_sharders`, keyed under `PECEmbeddingCollection` in `get_module_to_default_sharders` (distinct from `EmbeddingCollection`), so it only matches PEC modules and leaves other models unaffected.
    - Declare the `pec_embedding` dep on the `sharding_plan` BUCK target (the new import).
    - Update `test_module_to_default_sharders` to expect `PECEmbeddingCollection` in the default sharder map and assert its sharder type.
    
    Reviewed By: TroyGarden
    
    Differential Revision: D110148596
    
    fbshipit-source-id: d35c48c4119f6832ff5a0f5dfe65a291b0c542f4

--- torchrec/distributed/sharding_plan.py
+++ torchrec/distributed/sharding_plan.py
@@ -28,6 +28,7 @@ from torchrec.distributed.mc_embeddingbag import (
     ManagedCollisionEmbeddingBagCollectionSharder,
 )
 from torchrec.distributed.mc_modules import InferManagedCollisionCollectionSharder
+from torchrec.distributed.pec_embedding import PECEmbeddingCollectionSharder
 from torchrec.distributed.planner.constants import MIN_CW_DIM
 from torchrec.distributed.quant_embedding import (
     QuantEmbeddingCollectionSharder,
@@ -50,6 +51,7 @@ def get_default_sharders() -> List[ModuleSharder[nn.Module]]:
         cast(ModuleSharder[nn.Module], EmbeddingBagCollectionSharder()),
         cast(ModuleSharder[nn.Module], FeatureProcessedEmbeddingBagCollectionSharder()),
         cast(ModuleSharder[nn.Module], EmbeddingCollectionSharder()),
+        cast(ModuleSharder[nn.Module], PECEmbeddingCollectionSharder()),
         cast(ModuleSharder[nn.Module], FusedEmbeddingBagCollectionSharder()),
         cast(ModuleSharder[nn.Module], QuantEmbeddingBagCollectionSharder()),
         cast(ModuleSharder[nn.Module], QuantEmbeddingCollectionSharder()),
